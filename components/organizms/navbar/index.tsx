00cced84b5f232d01b9aa886f068ee106041ea02
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-20 09:59:05 +0700

    refactor login navbar component

--- components/organizms/navbar/index.tsx
+++ components/organizms/navbar/index.tsx
@@ -1,45 +1,36 @@
 import React from "react";
 import Image from "next/image";
 import Menu from "./menu";
+import Auth from "./auth";
+import ToggleMenu from "./toggleMenu";
 
 export default function Navbar() {
 	return (
-		<section>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
-				<div className="container-fluid">
-					<a className="navbar-brand" href="/#">
-						<Image src="/icon/logo.svg" width={60} height={60} alt={"logo"} />
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarNav"
-						aria-controls="navbarNav"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div className="collapse navbar-collapse" id="navbarNav">
-						<ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
-							<Menu title={"Home"} active href={"/"} />
-							<Menu title={"Games"} href={"/"} />
-							<Menu title={"Rewards"} href={"/"} />
-							<Menu title={"Discover"} href={"/"} />
-							<Menu title={"Global Rank"} href={"/"} />
-
-							<li className="nav-item my-auto">
-								<a
-									className="btn btn-sign-in d-flex justify-content-center ms-lg-2 rounded-pill"
-									href="./src/sign-in.html"
-									role="button">
-									Sign In
-								</a>
-							</li>
-						</ul>
+		<>
+			<section>
+				<nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
+					<div className="container-fluid">
+						<a className="navbar-brand" href="/#">
+							<Image src="/icon/logo.svg" width={60} height={60} alt={"logo"} />
+						</a>
+						<ToggleMenu />
+						<div className="collapse navbar-collapse" id="navbarNav">
+							<ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
+								<Menu title={"Home"} active href={"/"} />
+								<Menu title={"Games"} href={"/"} />
+								<Menu title={"Rewards"} href={"/"} />
+								<Menu title={"Discover"} href={"/"} />
+								<Menu title={"Global Rank"} href={"/"} />
+								<Auth isLogin />
+							</ul>
+						</div>
 					</div>
-				</div>
-			</nav>
-		</section>
+				</nav>
+			</section>
+			<script
+				src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+				integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+				crossOrigin="anonymous"></script>
+		</>
 	);
 }
