4c3e17a5cbaa9f7c18d47aaa76d84c3eca44517f
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-27 20:40:02 +0700

    set sign up action

--- components/organizms/SignUpForm/index.tsx
+++ components/organizms/SignUpForm/index.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import cx from "classnames";
+import { useRouter } from "next/router";
 
 export default function SignUpForm() {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const router = useRouter();
+
+	const className = {
+		label: cx("form-label text-lg fw-medium color-palette-1 mb-10"),
+	};
+
+	const onSubmit = () => {
+		console.log("email", email);
+		console.log("name", name);
+		console.log("password", password);
+		const userForm = {
+			email,
+			name,
+			password,
+		};
+
+		localStorage.setItem("user-form", JSON.stringify(userForm));
+		router.push("/sign-up-photo");
+	};
+
 	return (
 		<form action="">
 			<div className="pb-50">
@@ -13,16 +38,16 @@ export default function SignUpForm() {
 			<h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign Up</h2>
 			<p className="text-lg color-palette-1 m-0">Daftar dan bergabung dengan kami</p>
 			<div className="pt-50">
-				<label htmlFor="name" className="form-label text-lg fw-medium color-palette-1 mb-10">
+				<label htmlFor="name" className={className.label}>
 					Full Name
 				</label>
 				<input
 					type="text"
 					className="form-control rounded-pill text-lg"
-					id="name"
-					name="name"
 					aria-describedby="name"
 					placeholder="Enter your name"
+					value={name}
+					onChange={(event) => setName(event.target.value)}
 				/>
 			</div>
 			<div className="pt-30">
@@ -32,10 +57,10 @@ export default function SignUpForm() {
 				<input
 					type="email"
 					className="form-control rounded-pill text-lg"
-					id="email"
-					name="email"
 					aria-describedby="email"
 					placeholder="Enter your email address"
+					value={email}
+					onChange={(event) => setEmail(event.target.value)}
 				/>
 			</div>
 			<div className="pt-30">
@@ -45,19 +70,19 @@ export default function SignUpForm() {
 				<input
 					type="password"
 					className="form-control rounded-pill text-lg"
-					id="password"
-					name="password"
 					aria-describedby="password"
 					placeholder="Your password"
+					value={password}
+					onChange={(event) => setPassword(event.target.value)}
 				/>
 			</div>
 			<div className="button-group d-flex flex-column mx-auto pt-50">
-				<Link
+				<button
 					className="btn btn-sign-up fw-medium text-lg text-white rounded-pill mb-16"
-					href="/sign-up-photo"
-					role="button">
+					onClick={onSubmit}
+					type="button">
 					Continue
-				</Link>
+				</button>
 				<Link className="btn btn-sign-in fw-medium text-lg color-palette-1 rounded-pill" href="/sign-in" role="button">
 					Sign In
 				</Link>
--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Image from "next/image";
+import { getGameCategory } from "@/services/player";
+import { setSignUp } from "@/services/auth";
 
 export default function SignUpPhoto() {
+	const [categories, setCategories] = useState([]);
+	const [favorite, setFavorite] = useState("");
+	const [image, setImage] = useState("");
+	const [imagePreview, setImagePreview] = useState("/icon/uploadProfile.svg");
+	const [localForm, setLocalForm] = useState({ name: "", email: "" });
+
+	const getGameCategoryAPI = useCallback(async () => {
+		const data = await getGameCategory();
+		setCategories(data);
+		setFavorite(data[0]._id);
+	}, [getGameCategory]);
+
+	useEffect(() => {
+		getGameCategoryAPI();
+	}, []);
+
+	useEffect(() => {
+		const getLocalForm = localStorage.getItem("user-form");
+		setLocalForm(JSON.parse(getLocalForm));
+	}, []);
+
+	const onSubmit = async () => {
+		const getLocalForm = await localStorage.getItem("user-form");
+		const form = JSON.parse(getLocalForm);
+		const data = new FormData();
+
+		data.append("image", image);
+		data.append("name", form.name);
+		data.append("email", form.email);
+		data.append("password", form.password);
+		data.append("username", form.name);
+		data.append("phoneNumber", "0000000000");
+		data.append("favorite", favorite);
+
+		const result = await setSignUp(data);
+		console.log(result);
+	};
+
 	return (
 		<section className="sign-up-photo mx-auto pt-lg-227 pb-lg-227 pt-130 pb-50">
 			<div className="container mx-auto">
@@ -11,13 +51,23 @@ export default function SignUpPhoto() {
 							<div className="mb-20">
 								<div className="image-upload text-center">
 									<label htmlFor="avatar">
-										<Image src="/icon/uploadProfile.svg" width={120} height={120} alt={"logo"} />
+										<Image src={imagePreview} className="img-upload" width={120} height={120} alt={"logo"} />
 									</label>
-									<input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+									<input
+										id="avatar"
+										type="file"
+										name="avatar"
+										accept="image/png, image/jpeg"
+										onChange={(event) => {
+											const img = event.target.files[0];
+											setImagePreview(URL.createObjectURL(img));
+											setImage(img);
+										}}
+									/>
 								</div>
 							</div>
-							<h2 className="fw-bold text-xl text-center color-palette-1 m-0">Shayna Anne</h2>
-							<p className="text-lg text-center color-palette-1 m-0">shayna@anne.com</p>
+							<h2 className="fw-bold text-xl text-center color-palette-1 m-0">{localForm.name}</h2>
+							<p className="text-lg text-center color-palette-1 m-0">{localForm.email}</p>
 							<div className="pt-50 pb-50">
 								<label htmlFor="category" className="form-label text-lg fw-medium color-palette-1 mb-10">
 									Favorite Game
@@ -26,25 +76,27 @@ export default function SignUpPhoto() {
 									id="category"
 									name="category"
 									className="form-select d-block w-100 rounded-pill text-lg"
-									aria-label="Favorite Game">
-									<option value="" disabled selected>
-										Select Category
-									</option>
-									<option value="fps">First Person Shoter</option>
-									<option value="rpg">Role Playing Game</option>
-									<option value="arcade">Arcade</option>
-									<option value="sport">Sport</option>
+									aria-label="Favorite Game"
+									value={favorite}
+									onChange={(event) => setFavorite(event.target.value)}>
+									{categories.map((category) => {
+										return (
+											<option key={category._id} value={category._id}>
+												{category.name}
+											</option>
+										);
+									})}
 								</select>
 							</div>
 						</div>
 
 						<div className="button-group d-flex flex-column mx-auto">
-							<a
+							<button
 								className="btn btn-create fw-medium text-lg text-white rounded-pill mb-16"
-								href="/sign-up-photo-success"
-								role="button">
+								type="button"
+								onClick={onSubmit}>
 								Create My Account
-							</a>
+							</button>
 							<a className="btn btn-tnc text-lg color-palette-1 text-decoration-underline pt-15" href="#" role="button">
 								Terms & Conditions
 							</a>
