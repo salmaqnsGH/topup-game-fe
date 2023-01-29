1c2748ad1db562eae4a2df9ec1e969a84a8b5cab
salmaqnsGH <nurussalamahqonaah@gmail.com>
2023-01-29 10:47:04 +0700

    integrate checkout action

--- components/organizms/CheckoutConfirmation/index.tsx
+++ components/organizms/CheckoutConfirmation/index.tsx
@@ -1,20 +1,54 @@
-import React from "react";
+import { setCheckout } from "@/services/player";
+import router from "next/router";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function CheckoutConfirmation() {
+	const [checkbox, setCheckbox] = useState(false);
+
+	const onSubmit = async () => {
+		const dataItemLocal = localStorage.getItem("data-item");
+		const dataTopupLocal = localStorage.getItem("data-topup");
+
+		const dataItem = JSON.parse(dataItemLocal!);
+		const dataTopup = JSON.parse(dataTopupLocal!);
+
+		if (!checkbox) {
+			toast.error("Pastikan anda sudah melakukan pembayaran");
+		}
+
+		const data = {
+			voucher: dataItem._id,
+			nominal: dataTopup.nominalItem._id,
+			payment: dataTopup.paymentItem.payment._id,
+			bank: dataTopup.paymentItem.bank._id,
+			name: dataTopup.bankAccountName,
+			accountUser: dataTopup.verifyID,
+		};
+
+		const result = await setCheckout(data);
+		if (result.error) {
+			toast.error(result.message);
+		} else {
+			toast.success("checkout berhasil");
+			router.push("/complete-checkout");
+		}
+	};
+
 	return (
 		<>
 			<label className="checkbox-label text-lg color-palette-1">
 				I have transferred the money
-				<input type="checkbox" />
+				<input type="checkbox" checked={checkbox} onChange={() => setCheckbox(!checkbox)} />
 				<span className="checkmark"></span>
 			</label>
 			<div className="d-md-block d-flex flex-column w-100 pt-50">
-				<a
+				<button
 					className="btn btn-confirm-payment rounded-pill fw-medium text-white border-0 text-lg"
-					href="/complete-checkout"
-					role="button">
+					type="button"
+					onClick={onSubmit}>
 					Confirm Payment
-				</a>
+				</button>
 			</div>
 		</>
 	);
--- config/api/index.ts
+++ config/api/index.ts
@@ -1,7 +1,24 @@
 import axios, { AxiosRequestConfig } from "axios";
+import Cookies from "js-cookie";
 
-export default async function callAPI({ url, method, data }: AxiosRequestConfig) {
-	const response = await axios({ url, method, data }).catch((err) => err.response);
+interface CallAPIProps extends AxiosRequestConfig {
+	token?: boolean;
+}
+
+export default async function callAPI({ url, method, data, token }: CallAPIProps) {
+	let headers = {};
+
+	if (token) {
+		const tokenCookies = Cookies.get("token");
+		if (tokenCookies) {
+			const jwtToken = atob(tokenCookies);
+			headers = {
+				Authorization: `Bearer ${jwtToken}`,
+			};
+		}
+	}
+
+	const response = await axios({ url, method, data, headers }).catch((err) => err.response);
 
 	if (response.status > 300) {
 		const res = {
